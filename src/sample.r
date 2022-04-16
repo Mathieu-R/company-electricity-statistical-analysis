dc1f21667c3f0b877262287c7e2490b418e983e3
Mathieu-R <mathieu0709@gmail.com>
2022-04-16 20:12:38 +0200

    gini coefficient ok

--- src/sample.r
+++ src/sample.r
@@ -3,7 +3,21 @@
 # F^{-1}(y) = (-1/y)^{1/3}
 
 number_of_samples <- 20
-U <- runif(number_of_samples)
-X <- (-1/U)^(1/3)
 
+# generate randoms numbers from the uniform distribution U(0,1)
+U <- runif(number_of_samples)
+
+# compute X = F^{-1}(u)
+X <- 1 / (1-U)^(1/3)
+
+X
+
+# plot an histogram
 hist(X, freq=F, xlab="X", main="random sample")
+
+# compute Gini coefficients
+G_MLE <- 1 / ((2 * number_of_samples) / (sum(log(X / min(X)))) - 1)
+G_MME <- 1 / ((2 * (number_of_samples) * mean(X) - min(X) )/ (number_of_samples * (mean(X) - min(X))) - 1)
+
+G_MLE
+G_MME
