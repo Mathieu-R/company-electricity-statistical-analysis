4b25ff58524e1b9eb00a328019645410af44ac16
Mathieu-R <mathieu0709@gmail.com>
2022-04-18 10:35:34 +0200

    update

--- src/sample.r
+++ src/sample.r
@@ -4,6 +4,9 @@
 # F(x) = \int_0^{x} 3/t^4 dt = -[t^{-3}]_0^{x} = - 1/x^3
 # F^{-1}(y) = (-1/y)^{1/3}
 
+theta_1 <- 3
+theta_2 <- 1
+
 number_of_samples <- 20
 
 # cumulative density function
@@ -38,6 +41,10 @@ gini_mme <- function(rv_vector) {
   return (1 / ((2 * (number_of_samples) * mean(rv_vector) - min(rv_vector) )/ (number_of_samples * (mean(rv_vector) - min(rv_vector))) - 1))
 }
 
+theoretical_gini <- function(theta_1) {
+  return (1 / ((2 * theta_1) - 1))
+}
+
 rv_vector = generate_random_variables_vector(number_of_samples = number_of_samples, inv_cdf = inv_cdf)
 
 # plot an histogram of the random variable vector
@@ -55,6 +62,7 @@ gini_mle_sample <- numeric(number_of_iterations)
 gini_mme_sample <- numeric(number_of_iterations)
 
 for (i in 1:number_of_iterations) {
+  set.seed(i)
   # generate random variables
   rv_vector <- generate_random_variable(number_of_samples = number_of_samples, inv_cdf = inv_cdf)
 
@@ -62,17 +70,32 @@ for (i in 1:number_of_iterations) {
   gini_mle <- gini_mle(rv_vector = rv_vector)
   gini_mme <- gini_mme(rv_vector = rv_vector)
 
-  G_MLE_array[i] <- gini_mle
-  G_MME_array[i] <- gini_mme
+  gini_mle_sample[i] <- gini_mle
+  gini_mme_sample[i] <- gini_mme
 }
 
 hist(gini_mle_sample, main="", xlab="Gini MLE & MME samples", col="steelblue")
 hist(gini_mme_sample, col="red", add=TRUE)
 legend('topright', c('MLE', 'MME'), fill=c('steelblue', 'red'))
 
-boxplot(gini_mle_sample)
-boxplot(gini_mme_sample)
+boxplot(gini_mle_sample, col="grey"); abline(h=theoretical_gini(theta_1), col="brown", lty=2)
+boxplot(gini_mme_sample, col="grey"); abline(h=theoretical_gini(theta_1), col="brown", lty=2)
 
 # Bias, Variance and Mean Squared Error of these samples
+gini_mle_sample_bias <- mean(gini_mle_sample) - theoretical_gini(theta_1)
+gini_mme_sample_bias <- mean(gini_mme_sample) - theoretical_gini(theta_1)
+
 gini_mle_sample_variance <- sd(gini_mle_sample)^2
 gini_mme_sample_variance <- sd(gini_mme_sample)^2
+
+gini_mle_sample_mse <- mean((gini_mle_sample - theoretical_gini(theta_1))^2)
+gini_mme_sample_mse <- mean((gini_mme_sample - theoretical_gini(theta_1))^2)
+
+gini_mle_sample_bias
+gini_mme_sample_bias
+
+gini_mle_sample_variance
+gini_mme_sample_variance
+
+gini_mle_mse
+gini_mme_mse
